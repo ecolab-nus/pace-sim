a52c72b5ea2c9d34bdead05cb913ab3528ba2176
zhenyu bai <zhenyu.bai@nus.edu.sg>
2025-06-06 17:11:10 +0800

    add agu features

--- src/agu/state.rs
+++ src/agu/state.rs
@@ -1,7 +1,30 @@
-use super::instruction::Instruction;
+use crate::isa::pe::DMemInterface;
+
+use super::instruction::{InstMode, Instruction};
 
 pub struct AGUState {
     pub pc: u32,
     pub cm: Vec<Instruction>,
     pub arf: Vec<u16>,
+}
+
+impl AGUState {
+    pub fn new(cm: Vec<Instruction>, arf: Vec<u16>) -> Self {
+        Self { pc: 0, cm, arf }
+    }
+
+    pub fn step(&mut self, dmem: &mut DMemInterface) {
+        let inst = &self.cm[self.pc as usize];
+        let pc = self.pc as usize;
+        let addr = self.arf[pc];
+        dmem.wire_dmem_addr = Some(addr as u64);
+        match inst.inst_mode {
+            InstMode::STRIDED => {
+                self.arf[pc] = addr + inst.stride as u16;
+            }
+            InstMode::CONST => {
+                // do nothing
+            }
+        }
+    }
 }
